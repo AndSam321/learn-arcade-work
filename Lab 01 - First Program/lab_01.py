44c21139d8ea98060b405dbb4de56a1409d7e124
Andrew Samountry <andrew.samountry@my.simpson.edu>
2021-09-02 15:15:08 -0500

    Work for Lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("My name is Andrew.")
+print("I\nplan\non\nmajoring\nin\ncomputer\nscience!")
+print("""I'm from Arkansas, so I am a little far from home""")
+print("But I'm ready to start the year strong!")
