5ab0df706c5571133351af89c84e8d10626aaecf
unknown <andrew.samountry@my.simpson.edu>
2021-09-23 12:44:03 -0500

    Work on Lab 3

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -22,7 +22,24 @@ def draw_sun():
 
 # Birds
 def draw_birds(x, y):
-    arcade.draw_arc_outline(x, y, 100, 100, arcade.color.BLACK, 0, 180, 3)
+    arcade.draw_arc_outline(x, y, 100, 100, arcade.color.BLACK, 10, 70, 5)
+    arcade.draw_arc_outline(x, y, 100, 100, arcade.color.BLACK, 110, 170, 5)
+    arcade.draw_circle_filled(x, y + 40, 20, arcade.color.BLACK)
+
+
+# Boats
+def draw_boat(x, y):
+    arcade.draw_arc_filled(x, y, 200, 100, arcade.color.DARK_BROWN, 180, 360)
+    arcade.draw_rectangle_filled(x - 30, y + 50, 10, 100, arcade.color.BROWN)
+    arcade.draw_rectangle_filled(x - 10, y + 90, 50, 30, arcade.color.RED)
+
+
+# Ball
+def draw_basketball(x, y):
+    arcade.draw_circle_filled(x, y, 40, arcade.color.ORANGE)
+    arcade.draw_arc_outline(x, y, 75, 70, arcade.color.BLACK, 0, 120, 3, 90)
+    arcade.draw_arc_outline(x + 20, y, 67, 70, arcade.color.BLACK, 0, 180, 3, 85)
+    arcade.draw_arc_outline(x, y - 10, 75, 70, arcade.color.BLACK, 0, 180, 3)
 
 
 def main():
@@ -33,7 +50,13 @@ def main():
     draw_sand()
     draw_sunset()
     draw_sun()
-    draw_birds(50, 50)
+    draw_birds(300, 400)
+    draw_birds(500, 500)
+    draw_birds(600, 400)
+    draw_boat(200, 200)
+    draw_boat(600, 250)
+    draw_basketball(100, 100)
+    draw_basketball(200, 210)
 
     # Finishing
     arcade.finish_render()
