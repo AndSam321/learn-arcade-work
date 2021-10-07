4c21ab143ca7a670d5b3b9783dcf665b19299287
unknown <andrew.samountry@my.simpson.edu>
2021-10-07 00:02:05 -0500

    Work on Lab 5

--- Testing/test.py
+++ Testing/test.py
@@ -1,7 +1,4 @@
-def volume_cylinder(radius, height):
-    pi = 3.141592653
-    volume = pi * radius ** 2 * height
-    return volume
-
-my_volume = volume_cylinder(2.5, 5)
-print(my_volume)
+x = 1
+while x < 64:
+    print(x)
+    x = x * 2
